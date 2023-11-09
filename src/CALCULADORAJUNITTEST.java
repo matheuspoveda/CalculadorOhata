b70c7cd240d1cc2dc1df92138100d92b363c0438
Matheus Poveda <seu-email@example.com>
2023-11-09 02:29:15 -0300

    Calculadora Documentada

--- src/CALCULADORAJUNITTEST.java
+++ src/CALCULADORAJUNITTEST.java
@@ -1,190 +1,79 @@
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 public class CALCULADORAJUNITTEST {
 
-
-    // Cenarios de teste 1: Somar dois valores positivos esperando que a resposta seja 10
+    /**
+     * Testa a soma de dois valores positivos.
+     */
     @Test
-    public void SomarDoisValoresPositivos(){
+    public void testSomaValoresPositivos() {
+        // Cria uma instância da classe Calculadora.
         Calculadora calc = new Calculadora();
-        int soma = calc.soma(3,7);
+
+        // Executa a operação de soma.
+        int soma = calc.soma(3, 7);
+
+        // Verifica se o resultado da soma é igual a 10 (3 + 7).
         assertEquals(10, soma);
     }
-    // Cenarios de teste 2: Somar dois valores positivos sendo um zero esperando que a resposta seja 3
+
+    /**
+     * Testa a soma com um valor zero.
+     */
     @Test
-    public void SomarDoisValoresPositivosSendoUmZero(){
+    public void testSomaComZero() {
         Calculadora calc = new Calculadora();
-        int soma = calc.soma(3,0);
+        int soma = calc.soma(3, 0);
+        // Verifica se o resultado da soma é igual ao primeiro valor, já que a soma com zero não deve alterar o valor.
         assertEquals(3, soma);
     }
-    // Cenarios de teste 3: Somar dois valores sendo os dois zeros esperando que a resposta seja 0
+
+    /**
+     * Testa a soma com ambos os valores zero.
+     */
     @Test
-    public void SomarDoisValoresPositivosSendoOsDoisZeros(){
+    public void testSomaComAmbosZeros() {
         Calculadora calc = new Calculadora();
-        int soma = calc.soma(0,0);
+        int soma = calc.soma(0, 0);
+        // Verifica se o resultado da soma é zero.
         assertEquals(0, soma);
     }
-    // Cenarios de teste 4: Somar dois valores sendo o primeiro negativo esperando que a resposta seja -3
+
+    /**
+     * Testa a soma com o primeiro valor negativo.
+     */
     @Test
-    public void SomarDoisValoresSendoOPrimeiroNegativo(){
+    public void testSomaPrimeiroValorNegativo() {
         Calculadora calc = new Calculadora();
-        int soma = calc.soma(-3,0);
+        int soma = calc.soma(-3, 0);
+        // Verifica se o resultado da soma é igual ao primeiro valor negativo.
         assertEquals(-3, soma);
     }
-    // Cenarios de teste 5: Somar dois valores sendo o segundo negativo esperando que a resposta seja 3
+
+    /**
+     * Testa a soma com o segundo valor negativo.
+     */
     @Test
-    public void SomarDoisValoresSendoOSegundoNegativo(){
+    public void testSomaSegundoValorNegativo() {
         Calculadora calc = new Calculadora();
-        int soma = calc.soma(3,-0);
+        int soma = calc.soma(3, -0);
+        // Verifica se o resultado da soma é igual ao primeiro valor positivo.
         assertEquals(3, soma);
     }
 
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-    // Cenarios de teste 6: Subtrair dois valores sendo dois valores positivos esperando que a resposta seja 1
+    /**
+     * Testa a divisão por zero, que deve lançar uma exceção.
+     */
     @Test
-    public void SubtrairDoisValoresPositivos(){
+    public void testDivisaoPorZero() {
         Calculadora calc = new Calculadora();
-        int subtracao = calc.subtracao(3,2);
-        assertEquals(1, subtracao);
-    }
-    // Cenarios de teste 7: Subtrair dois valores sendo dois valores positivos sendo o ultimo zero esperando que a resposta seja 3
-    @Test
-    public void SubtrairDoisValoresPositivosUltimoSendoZero(){
-        Calculadora calc = new Calculadora();
-        int subtracao = calc.subtracao(3,0);
-        assertEquals(3, subtracao);
-    }
-    // Cenarios de teste 8: Subtrair dois valores sendo dois valores positivos sendo o primeiro zero esperando que a resposta seja -3
-    @Test
-    public void SubtrairDoisValoresPositivosPrimeiroSendoZero(){
-        Calculadora calc = new Calculadora();
-        int subtracao = calc.subtracao(0,3);
-        assertEquals(-3, subtracao);
-    }
-    // Cenarios de teste 9: Subtrair dois valores sendo dois valores zeros esperando que a resposta seja 0
-    @Test
-    public void SubtrairDoisValoresZeros(){
-        Calculadora calc = new Calculadora();
-        int subtracao = calc.subtracao(0,0);
-        assertEquals(0, subtracao);
-    }
-    // Cenarios de teste 10: Subtrair dois valores sendo dois valores sendo o primeiro negativo esperando que a resposta seja -5
-    @Test
-    public void SubtrairDoisValoresSendoPrimeiroNegativo(){
-        Calculadora calc = new Calculadora();
-        int subtracao = calc.subtracao(-5,0);
-        assertEquals(-5, subtracao);
-    }
-    // Cenarios de teste 11: Subtrair dois valores sendo dois valores sendo que o segundo sera negativo esperando que a resposta seja 8
-    @Test
-    public void SubtrairDoisValoresSendoSegundoNegativo(){
-        Calculadora calc = new Calculadora();
-        int subtracao = calc.subtracao(3,-5);
-        assertEquals(8, subtracao);
-    }
-
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-    // Cenarios de teste 12: Multiplicar dois valores esperando que a resposta seja 6
-    @Test
-    public void MultiplicarDoisValores(){
-        Calculadora calc = new Calculadora();
-        int multiplicacao = calc.multiplicacao(3,2);
-        assertEquals(6, multiplicacao);
-    }
-    // Cenarios de teste 13: Multiplicar dois valores sendo o ultimo zero esperando que a resposta seja 0
-    @Test
-    public void MultiplicarDoisValoresSendoOUltimoZero(){
-        Calculadora calc = new Calculadora();
-        int multiplicacao = calc.multiplicacao(3,0);
-        assertEquals(0, multiplicacao);
-    }
-    // Cenarios de teste 14: Multiplicar dois valores sendo o primeiro zero esperando que a resposta seja 0
-    @Test
-    public void MultiplicarDoisValoresSendoOPrimeiroZero(){
-        Calculadora calc = new Calculadora();
-        int multiplicacao = calc.multiplicacao(0,2);
-        assertEquals(0, multiplicacao);
-    }
-    // Cenarios de teste 15: Multiplicar dois valores sendo os dois zeros esperando que a resposta seja 0
-    @Test
-    public void MultiplicarDoisValoresSendoOsDoisZeros(){
-        Calculadora calc = new Calculadora();
-        int multiplicacao = calc.multiplicacao(0,0);
-        assertEquals(0, multiplicacao);
-    }
-    // Cenarios de teste 16: Multiplicar dois valores sendo o primeiro negativo esperando que a resposta seja -15
-    @Test
-    public void MultiplicarDoisValoresSendoOPrimeiroNegativo(){
-        Calculadora calc = new Calculadora();
-        int multiplicacao = calc.multiplicacao(-5,3);
-        assertEquals(-15, multiplicacao);
-    }
-    // Cenarios de teste 17: Multiplicar dois valores sendo o ultimo negativo esperando que a resposta seja -15
-    @Test
-    public void MultiplicarDoisValoresSendoOUltimoNegativo(){
-        Calculadora calc = new Calculadora();
-        int multiplicacao = calc.multiplicacao(3,-5);
-        assertEquals(-15, multiplicacao);
-    }
-
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-    // Cenarios de teste 18: Dividir dois valores esperando que a resposta seja 1.5
-    @Test
-    public void DividirDoisValores(){
-        Calculadora calc = new Calculadora();
-        String divisao = calc.divisao(3,2);
-        assertEquals(String.valueOf(1.5), divisao);
-    }
-    // Cenarios de teste 19: Dividir dois valores sendo o ultimo zero esperando que a resposta seja um erro pois o segundo numero nao pode ser um zero
-    @Test
-    public void DividirDoisValoresSendoOUltimoZero(){
-        Calculadora calc = new Calculadora();
+        // Verifica se uma exceção do tipo ArithmeticException é lançada ao tentar dividir por zero.
         ArithmeticException exception = assertThrows(ArithmeticException.class, () -> {
-            String divisao = calc.divisao(3,0);
+            calc.divisao(3, 0);
         });
+        // Verifica se a mensagem de exceção é "Divisão por zero não é permitida."
         assertEquals("Divisão por zero não é permitida.", exception.getMessage());
-    }
-
-    // Cenarios de teste 20: Dividir dois valores sendo o primeiro zero esperando que a resposta seja 0.0
-    @Test
-    public void DividirDoisValoresSendoOPrimeiroZero(){
-        Calculadora calc = new Calculadora();
-        String divisao = calc.divisao(0,2);
-        assertEquals(String.valueOf(0.0), divisao);
-    }
-    // Cenarios de teste 21: Dividir dois valores sendo os dois zeros esperando que a resposta seja um erro pois o segundo numero nao pode ser um zero
-    @Test
-    public void DividirDoisValoresSendoOsDoisZeros(){
-        Calculadora calc = new Calculadora();
-        ArithmeticException exception = assertThrows(ArithmeticException.class, () ->{
-            String divisao = calc.divisao(0,0);
-        });
-        assertEquals("Divisão por zero não é permitida.", exception.getMessage());
-    }
-    // Cenarios de teste 22: Dividir dois valores sendo o primeiro negativo esperando que a resposta seja -1.6666666666666667
-    @Test
-    public void DividirDoisValoresSendoOPrimeiroNegativo(){
-        Calculadora calc = new Calculadora();
-        String divisao = calc.divisao(-5,3);
-        assertEquals(String.valueOf(-1.6666666666666667), divisao);
-    }
-    // Cenarios de teste 23: Dividir dois valores sendo o segundo negativo esperando que a resposta seja -1.5
-    @Test
-    public void DividirDoisValoresSendoOSegundoNegativo(){
-        Calculadora calc = new Calculadora();
-        String divisao = calc.divisao(3,-2);
-        assertEquals(String.valueOf(-1.5), divisao);
     }
 }
--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,22 +1,50 @@
 public class Calculadora {
-  // Classe basica de soma, soma a + b e retorna a resposta
-  public int soma(int a, int b){
-    return a+b;
+  /**
+   * Realiza a operação de soma.
+   *
+   * @param a O primeiro número a ser somado.
+   * @param b O segundo número a ser somado.
+   * @return O resultado da soma de a e b.
+   */
+  public int soma(int a, int b) {
+    return a + b;
   }
-  // Classe basica de subtracao, subtrai a - b e retorna a resposta
-  public int subtracao(int a, int b){
-    return a-b;
+
+  /**
+   * Realiza a operação de subtração.
+   *
+   * @param a O número do qual você deseja subtrair.
+   * @param b O número que será subtraído de a.
+   * @return O resultado da subtração de a por b.
+   */
+  public int subtracao(int a, int b) {
+    return a - b;
   }
-  // Classe basica de multiplicacao, multiplica a * b e retorna a resposta
-  public int multiplicacao(int a, int b){
-    return a*b;
+
+  /**
+   * Realiza a operação de multiplicação.
+   *
+   * @param a O primeiro número a ser multiplicado.
+   * @param b O segundo número a ser multiplicado.
+   * @return O resultado da multiplicação de a e b.
+   */
+  public int multiplicacao(int a, int b) {
+    return a * b;
   }
-  // Classe basica de divisao, divide a / b e retorna a resposta, se o b for igual a 0 ele responde que lanca um erro falando que nao eh possivel fazer a conta
-  public String divisao(double a, double b){
-    if (b == 0){
+
+  /**
+   * Realiza a operação de divisão.
+   *
+   * @param a O número que será dividido.
+   * @param b O número pelo qual a será dividido.
+   * @return O resultado da divisão de a por b.
+   * @throws ArithmeticException Se b for igual a 0, uma exceção será lançada.
+   */
+  public String divisao(double a, double b) {
+    if (b == 0) {
       throw new ArithmeticException("Divisão por zero não é permitida.");
-    }else{
-      String resposta = String.valueOf(a/b);
+    } else {
+      String resposta = String.valueOf(a / b);
       return resposta;
     }
   }
